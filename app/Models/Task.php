28ab4a31f41dac5fab626346e0cb3336ba0a2660
Muhamaddeas <muhamaddeas7@gmail.com>
2025-12-04 10:55:10 -0800

    menambahkan database & model task

---
+++ app/Models/Task.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    protected $fillable = [
+        'name'
+        'status'
+        'priority'
+        'due_date'
+    ];
+
+    protected $casts = [
+        'status' => 'boolean',
+        'due_date' => 'date',
+    ];
+
+    protected $casts = [
+        'status' => false,
+        'priority' => 3,
+    ];
+
+    
+}
---
+++ database/migrations/2025_12_03_211648_create_tasks_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string(colum: 'name');
+            $table->boolean(colum: 'status')->default(false);
+            $table->intiger(colum: 'priority')->default(3);
+            $table->date(colum: 'due_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
